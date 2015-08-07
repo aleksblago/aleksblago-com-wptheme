3f39c3ec3091d67afcd9455e44b7be3bc5deb792
Aleks Blagojevich <me@aleksblago.com>
2015-08-07 00:42:57 -0700

    Adjusting search input styles.

--- navigation.php
+++ navigation.php
@@ -6,7 +6,7 @@
 			</header>
 			<div class="nav-Search">
 				<form role="search" method="get" class="nav-SearchForm" name="search" action="<?php echo home_url( '/' ); ?>">
-					<input type="text" name="s" class="nav-SearchInput" placeholder="Search for stuff...">
+					<input type="text" name="s" class="nav-SearchInput" placeholder="Search">
 				</form>	
 			</div>
 			<footer class="nav-Footer">
