00b2f95ab67a0ffa78303d2f832c0a12dbcae2cd
Aleks Blagojevich <me@aleksblago.com>
2015-08-07 01:33:05 -0700

    Add category and category link to article posts.

--- loop.php
+++ loop.php
@@ -53,9 +53,17 @@
 			// IF we're on the default homepage, on a dynamic blog page, on an archive page, or on a single post page.
 			// Basically, exclude the author and date on a static homepage.
 		?>
-		<?php if ((is_front_page() && is_home()) || is_home() || is_archive() || is_search() || is_single()) : ?>
+		<?php // if ((is_front_page() && is_home()) || is_home() || is_archive() || is_search() || is_single()) : ?>
+		<?php if (!is_page()) : ?>
+		<?php
+			$categories = get_the_category();
+			$the_category = $categories[0]->cat_name;
+			$category_id = get_cat_ID( $the_category );
+			$category_link = get_category_link( $category_id );
+		?>
 			<div class="ui-PostMeta">
 				<span><i class="fa fa-fw fa-user"></i> <?php the_author_posts_link(); ?></span>
+				<span><i class="fa fa-fw fa-folder-open"></i> <a href="<?php echo $category_link; ?>" title="<?php the_title(); ?> is categorized in <?php echo $the_category; ?>"><?php echo $the_category; ?></a></span>
 				<span><i class="fa fa-fw fa-clock-o"></i> <?php echo do_shortcode('[est_time]'); ?></span>
 				<span><i class="fa fa-fw fa-calendar"></i> <?php the_date(); ?></span>
 			</div>
