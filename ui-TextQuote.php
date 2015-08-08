9654fa78219769cee5d4e216cad39bf1fa25ecc4
Aleks Blagojevich <me@aleksblago.com>
2015-08-08 15:54:30 -0700

    Add schema.org microdata.

--- ui-TextQuote.php
+++ ui-TextQuote.php
@@ -1,12 +1,12 @@
 <?php if (!is_single()) : ?>
 	
 	<a href="<?php the_permalink(); ?>" class="ui-TextQuote" itemprop="url">
-		<?php the_content(); ?>
+		<span itemprop="articleBody"><?php the_content(); ?></span>
 	</a>
 	
 <?php else : ?>
 	
-	<div class="ui-TextQuote">
+	<div class="ui-TextQuote" itemprop="articleBody">
 		<?php the_content(); ?>
 	</div>
 
