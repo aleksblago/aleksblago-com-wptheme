98fe6bcc7aa855b4d166739602792d781baa3d92
Aleks Blagojevich <me@aleksblago.com>
2015-08-08 06:47:39 -0700

    Remove device specific code. Its a bit heavy handed at this point.

--- src/js/aleksblago.js
+++ src/js/aleksblago.js
@@ -1,20 +1,16 @@
 (function() {
 	
-	if (device.type === 'mobile') {
+	var menuBtn = document.querySelector('.nav-MobileMenu'),
+		navBar = document.querySelector('.nav-Links'),
+		navSearch = document.querySelector('.nav-Search'),
+		navFooter = document.querySelector('.nav-Footer');
+	
+	menuBtn.addEventListener('click', function() {
 		
-		var menuBtn = document.querySelector('.nav-MobileMenu'),
-			navBar = document.querySelector('.nav-Links'),
-			navSearch = document.querySelector('.nav-Search'),
-			navFooter = document.querySelector('.nav-Footer');
+		navBar.classList.toggle('active');
+		navSearch.classList.toggle('active');
+		navFooter.classList.toggle('active');
 		
-		menuBtn.addEventListener('click', function() {
-			
-			navBar.classList.toggle('active');
-			navSearch.classList.toggle('active');
-			navFooter.classList.toggle('active');
-			
-		}, false);
-		
-	}
+	}, false);
 	
 })();
