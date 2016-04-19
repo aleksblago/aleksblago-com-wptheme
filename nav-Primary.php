f0f3e0f91e16d7e6c06fa49e31e3186d55261f9e
Aleks Blagojevich <me@aleksblago.com>
2016-04-19 04:40:31 -0700

    Adding stack overflow to navigation.

--- nav-Primary.php
+++ nav-Primary.php
@@ -9,9 +9,9 @@
 		<ul class="nav-Social">
 			<li><a href="https://www.linkedin.com/in/aleksblago" target="_blank" itemprop="url"><i class="fa fa-fw fa-linkedin"></i></a></li>
 			<li><a href="https://twitter.com/_aleksblago" target="_blank" itemprop="url"><i class="fa fa-fw fa-twitter"></i></a></li>
-			<li><a href="http://www.last.fm/user/aleksblago" target="_blank" itemprop="url"><i class="fa fa-fw fa-lastfm"></i></a></li>
 			<li><a href="https://github.com/aleksblago" target="_blank" itemprop="url"><i class="fa fa-fw fa-github-alt"></i></a></li>
 			<li><a href="http://codepen.io/aleksblago/" target="_blank" itemprop="url"><i class="fa fa-fw fa-codepen"></i></a></li>
+			<li><a href="http://stackoverflow.com/users/973991/aleksblago" target="_blank" itemprop="url"><i class="fa fa-fw fa-stack-overflow"></i></a></li>
 		</ul>
 		&copy; 2004-<?php echo date('Y'); ?>
 	</footer>
